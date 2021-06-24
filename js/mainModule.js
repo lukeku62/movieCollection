5512d2990ea5bf43f75e70cbd2bea9aca4d7f131
lukeku62 <prevebbo@gmail.com>
2021-06-24 14:44:37 +0200

    removedAmp

--- js/mainModule.js
+++ js/mainModule.js
@@ -1,9 +1,7 @@
 import { trendingMovies, trendingTvSeries, searchMovie, searchTvSerie} from './util/fetchApi.js'
 import { makeMediaCard, getModal } from './util/dynamicCreation.js'
 import { storeQuery } from './util/storage.js'
-import { Amplify } from 'aws-amplify' 
-import { config } from '.aws-exports'
-Amplify.configure(config)
+
 
 //funzione che genera una lista di film di tendenza
 export const callTrendingMovies = async () => {
